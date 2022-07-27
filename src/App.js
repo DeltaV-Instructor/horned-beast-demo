83512749dd434fea0ac76862084de384c8ac42e5
tektechnologies <info@tektechnologies.com>
2022-07-27 16:18:33 -0500

    add demo updates

--- src/App.js
+++ src/App.js
@@ -2,17 +2,99 @@ import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import Main from './Main.js';
+import data from './data.json';
+import Modal from 'react-bootstrap/Modal';
 
 
 class App extends React.Component{
 
+// first lets add constructor
+constructor(props){
+  // built into class components for and how they work calling in all the things passed in
+  super(props);
+    this.state = {
+      // how do we pass state to another component? through props.
+      pizza: '',
+      //add state for modal
+      //1. modal
+      // showModal: true
+      showModal: false,
+      selectedPizza: ''
+    
+  };
+};
+
+// Now lets create an event handler to add pizzas
+addPizza = () => {
+  // everytime a click event happens I want to add more hearts 
+  this.setState({
+    pizza: this.state.pizza + '🍕'
+  });
+};
+
+
+
+
+handleOnHide = () => {
+  this.setState({
+    showModal: false
+  });
+};
+
+
+// so we still need a method to open the modal DO first then update with modal data
+// handleOnShowModal = () => {
+//   this.setState({
+//     showModal: true
+//   });
+// };
+// in pizza how do we get this data back? 
+handleOnShowModal = (name) => {
+  this.setState({
+    showModal: true,
+    selectedPizza: name
+  });
+};
+
+
+
+
+
 render() {
   return (
     <>
-    
-    <Header />
-    <Main />
+    {/* TEST the modal working  */}
+    {/* <p onClick={this.handleOnShowModal}>Open</p> */}
+
+    <Header pizzas={this.state.pizza} />
+
+    <Main   
+      addPizza={this.addPizza}
+      data={data} 
+      handleOnShowModal={this.handleOnShowModal}
+      />
+
     <footer>DeltaV, 2022</footer>
+
+   {/* this is where our selectedBeast component code will go  */}
+    <Modal  show={this.state.showModal}  onHide={this.handleOnHide} >
+
+        {/* <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header> */}
+
+        {/* then with data */}
+
+        <Modal.Header closeButton>
+          <Modal.Title>{this.state.selectedPizza}</Modal.Title>
+        </Modal.Header>
+
+
+    </Modal>
+
+
+
+
      </>
   );
  }
--- src/Main.js
+++ src/Main.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import Pizza from './Pizza.js';
 import './Main.css';
-import Data from './data.json';
+// object deconstruction 
+import { Container, Row } from 'react-bootstrap';
+
 
 class Main extends React.Component {
   render () {
-    //How do we get data out of our json.
-    console.log('json data', Data);
-    //do some js stuff before we return so lets build our component here
-    let pizzas = [];
-    Data.forEach((newPizza) => {
-      pizzas.push(<Pizza pizzaName={newPizza.name}  image_URL={newPizza.imageURL}/>)
+    // let pizzas = [];
+    let pizzas = this.props.data.map((newPizza, index) => {
+      // pizzas.push(
+        // return to escape code block 
+     return <Pizza
+       pizzaName={newPizza.name} 
+       key={index}
+       image_URL={newPizza.imageURL}
+       addPizza={this.props.addPizza}
+       handleOnShowModal={this.props.handleOnShowModal}
+
+      />
+      // )
     });
 
 
-      console.log('pizza components? ',pizzas)
 
-
-
-
-
-
+  //  see the function from app.js 
+    console.log('props from app.js',this.props);
     return(
 
       <main> 
-      {/* <Pizza pizzaName="Detroit"  />
-      <Pizza pizzaName="New York Thin"  />
-      <Pizza pizzaName="Chicago Deep Dish"/>
-      <Pizza pizzaName="Oven Grinder"/>
-      <Pizza pizzaName="Brick Oven"/>
-      <Pizza pizzaName="Calzone"/> */}
-      {pizzas}
-
+         <Container>
+          {/* show then customize the row  */}
+           <Row lg={4} md={3} sm={2} xs={1}>
+           {/* add column in pizza  */}
+            {pizzas}
+           </Row>
+        </Container>
       </main>
     );
   }
